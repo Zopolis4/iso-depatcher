5fc6fd35e7de27166fe53982f7f83318e32894c2
Zopolis4 <64618338+Zopolis4@users.noreply.github.com>
2021-05-26 10:21:50 +1000

    Use -O instead of creating subdirectory
    The use of the -O option in jdupes allows the correct files to be deleted, without the need to use a temporary subdirectory.

--- iso-depatcher.sh
+++ iso-depatcher.sh
@@ -1,12 +1,8 @@
 #! /bin/sh
 var1=$(echo -n "$(dirname "$1")/";echo "$(basename "$1" | sed 's/\(.*\)\..*/\1/')")
-var2=$(echo -n "$(dirname "$2")/patch/";echo "$(basename "$2" | sed 's/\(.*\)\..*/\1/')")
-var3=$(dirname $2)
-var4=$(echo "$(dirname "$2")/patch")
+var2=$(echo -n "$(dirname "$2")/";echo "$(basename "$2" | sed 's/\(.*\)\..*/\1/')")
 ./wit extract $1 $var1 -p -v -v -l
 ./wit extract $2 $var2 -p -v -v -l
-./jdupes -R -d -N $var1 $var2
+./jdupes -O -R -d -N $var1 $var2
 find $var2 -type d -empty -delete
-mv $var2 $var3
 rm -rf $var1
-rmdir $var4
