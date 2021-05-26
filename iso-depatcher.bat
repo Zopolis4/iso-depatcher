a2ca4de2c5521d2aa6bd447b2128296d8cd12af6
Zopolis4 <64618338+Zopolis4@users.noreply.github.com>
2021-05-26 10:20:19 +1000

    Use -O instead of creating subdirectory
    The use of the -O option in jdupes allows the correct files to be deleted, without the need to use a temporary subdirectory

--- iso-depatcher.bat
+++ iso-depatcher.bat
@@ -1,7 +1,5 @@
 wit extract %1 %~dp1%~n1 -p -v -v -l
-wit extract %2 %~dp2patch\%~n2 -p -v -v -l
-jdupes -R -d -N %~dp1%~n1 %~dp2patch\%~n2
-ROBOCOPY %~dp2patch\%~n2 %~dp2patch\%~n2 /S /MOVE
-MOVE %~dp2patch\%~n2 %~dp0
+wit extract %2 %~dp2%~n2 -p -v -v -l
+jdupes -O -R -d -N %~dp1%~n1 %~dp2%~n2
+ROBOCOPY %~dp2%~n2 %~dp2%~n2 /S /MOVE
 RMDIR /S /Q %~dp1%~n1
-RMDIR /S /Q %~dp2patch\
