599ce1fa7d0761330559fa7e9649660f3622f88c
Zopolis4 <64618338+Zopolis4@users.noreply.github.com>
2021-05-27 18:45:54 +1000

    Fix variables
    It should work now

--- other/bulk-depatcher.bat
+++ other/bulk-depatcher.bat
@@ -1,3 +1,3 @@
 wit extract %1 %~dp1%~n1 -p -v -v -l
-jdupes -O -R -d -N drive:extracted\unpatched\game %~dp2%~n2
-ROBOCOPY %~dp2%~n2 %~dp2%~n2 /S /MOVE
+jdupes -O -R -d -N drive:extracted\unpatched\game %~dp1%~n1
+ROBOCOPY %~dp1%~n1 %~dp1%~n1 /S /MOVE
